a113a27e1b79f9011a918970b8082f8856c19916
SandeepNagisetti <87413367+SandeepNagisetti@users.noreply.github.com>
2024-04-18 14:39:10 +0530

    Delete tests/sample.m

--- tests/sample.m
+++
@@ -1,9 +0,0 @@
-classdef SolverTest < matlab.unittest.TestCase
-    methods(Test)
-        function realSolution(testCase)
-            actSolution = quadraticSolver(1,-3,2);
-            expSolution = [2 1];
-            testCase.verifyEqual(actSolution,expSolution)
-        end
-    end
-end
